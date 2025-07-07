c6b6382f64c5082f03db3794859ea262b9433a8b
fersanchez1296 <fernandosanchezplacencia@gmail.com>
2025-07-07 11:45:20 -0600

    Se agrego la conexion websocket al servicio de notificaciones

--- src/context/SocketProvider.tsx
+++ src/context/SocketProvider.tsx
@@ -19,7 +19,7 @@ export function SocketProvider({ children }: { children: React.ReactNode }) {
 
   useEffect(() => {
     if (session?.user?.accessToken && !socket) {
-      const newSocket = io("http://localhost:4200", {
+      const newSocket = io("http://localhost:4700", {
         auth: {
           token: session.user.accessToken,
         },
